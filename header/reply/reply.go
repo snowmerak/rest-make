cde9e829ab6f4f8b4d66c525de3d4632d1d3ef04
snowmerak <snowmerak@outlook.com>
2022-01-11 15:20:44 +0900

    Update reply.go

--- header/reply/reply.go
+++ header/reply/reply.go
@@ -9,6 +9,16 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
+func Data(ctx *fasthttp.RequestCtx, contentType contenttype.ContentType, data []byte) error {
+	ctx.SetContentType(string(contentType))
+	if _, err := ctx.Write(data); err != nil {
+		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
+		return err
+	}
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	return nil
+}
+
 func JSON(ctx *fasthttp.RequestCtx, data interface{}) error {
 	ctx.SetContentType("application/json")
 	encoder := json.NewEncoder(ctx)
@@ -21,13 +31,43 @@ func JSON(ctx *fasthttp.RequestCtx, data interface{}) error {
 }
 
 func Protobuf(ctx *fasthttp.RequestCtx, data protoreflect.ProtoMessage) error {
-	ctx.SetContentType(string(contenttype.ApplicationProto))
+	ctx.SetContentType("application/protobuf")
 	buf, err := proto.Marshal(data)
 	if err != nil {
 		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
 		return err
 	}
 	if _, err := ctx.Write(buf); err != nil {
+		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
+		return err
+	}
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	return nil
+}
+
+func Plain(ctx *fasthttp.RequestCtx, data string) error {
+	ctx.SetContentType("text/plain")
+	if _, err := ctx.WriteString(data); err != nil {
+		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
+		return err
+	}
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	return nil
+}
+
+func HTML(ctx *fasthttp.RequestCtx, data []byte) error {
+	ctx.SetContentType("text/html")
+	if _, err := ctx.Write(data); err != nil {
+		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
+		return err
+	}
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	return nil
+}
+
+func Binary(ctx *fasthttp.RequestCtx, data []byte) error {
+	ctx.SetContentType("application/octet-stream")
+	if _, err := ctx.Write(data); err != nil {
 		ctx.SetStatusCode(fasthttp.StatusServiceUnavailable)
 		return err
 	}
