3c09ccecedd43df92c983d8f51aa2c8347e90c26
snowmerak <snowmerak@outlook.com>
2022-01-11 15:55:30 +0900

    Create auth.go

---
+++ auth/auth.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/snowmerak/dakuaz"
+	"github.com/snowmerak/rest-make/cookie/jwthmac"
+	"github.com/valyala/fasthttp"
+)
+
+const token = "token"
+
+func JWT(ctx *fasthttp.RequestCtx, criteria func(jwt.MapClaims) bool) error {
+	ck := ctx.Request.Header.Cookie(token)
+	if ck == nil {
+		ctx.Response.Header.SetStatusCode(fasthttp.StatusUnauthorized)
+		return fmt.Errorf("no token")
+	}
+	claims, err := jwthmac.Deserialize(string(ck))
+	if err != nil {
+		ctx.Response.Header.SetStatusCode(fasthttp.StatusUnauthorized)
+		return fmt.Errorf("auth.JWT: %w", err)
+	}
+	if !criteria(claims) {
+		ctx.Response.Header.SetStatusCode(fasthttp.StatusForbidden)
+		return fmt.Errorf("token is disqualified")
+	}
+	return nil
+}
+
+func Dakuaz(ctx *fasthttp.RequestCtx, criteria func(*dakuaz.Dakuaz) bool) error {
+	ck := ctx.Request.Header.Cookie(token)
+	if ck == nil {
+		return fmt.Errorf("no token")
+	}
+	d, err := dakuaz.Desirialize(string(ck))
+	if err != nil {
+		return fmt.Errorf("auth.Dakuaz: %w", err)
+	}
+	if !criteria(d) {
+		return fmt.Errorf("token is disqualified")
+	}
+	return nil
+}
